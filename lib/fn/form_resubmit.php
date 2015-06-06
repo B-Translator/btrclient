d386eba637c18453a9b5e752c793ae8fc0478ce6
Dashamir Hoxha <dashohoxha@gmail.com>
2015-06-06 09:40:35 +0000

    Reorganize authenticate and form_resubmit functions.

---
+++ lib/fn/form_resubmit.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @file
+ * Function: form_resubmit()
+ */
+
+namespace BTranslator\Client;
+use \bcl;
+
+/**
+ * If there is a $_SESSION['btrClient']['form_state'],
+ * then this is a redirect after login. Call the submit function again.
+ */
+function form_resubmit() {
+  if (isset($_SESSION['btrClient']['form_state'])) {
+    $form_state = $_SESSION['btrClient']['form_state'];
+    unset($_SESSION['btrClient']['form_state']);
+    $form_id = $form_state['values']['form_id'];
+    $submit = $form_id . '_submit';
+    $submit(NULL, $form_state);
+  }
+}
