a98df9a1afc30a3f66f06c2feb0e00ad0bdbea0c
Dashamir Hoxha <dashohoxha@gmail.com>
2015-06-08 17:25:31 +0000

    Reformat arrays.

--- lib/fn/translateform/buttons.php
+++ lib/fn/translateform/buttons.php
@@ -12,23 +12,21 @@ use \bcl;
  */
 function translateform_buttons($lng, $sguid = NULL) {
   if ($sguid and bcl::inside_iframe()) {
-    $buttons['details'] = array(
-      '#markup' => l('#', "translations/$lng/$sguid",
-                 array('attributes' => array(
-                     'class' => array('btn', 'form-submit'),
-                     'style' => 'font-weight:bold',
+    $buttons['details'] = [
+      '#markup' => l('#', "translations/$lng/$sguid", [
+                   'attributes' => [
+                     'class' => ['btn'],
+                     'style' => 'font-weight:bold; margin-right:3px;',
                      'target' => '_blank',
-                   ))),
-    );
+                   ]]),
+    ];
   }
-  $buttons['login'] = array(
+  $buttons['login'] = [
     '#type' => 'submit',
     '#value' => t('Login'),
     '#access' => !bcl::user_is_authenticated(),
-    '#attributes' => array(
-      'onclick' => 'this.form.target="_self"',
-    ),
-  );
+    '#attributes' => ['onclick' => 'this.form.target="_self"'],
+  ];
   // When we are inside an iFrame, it is better to do the login
   // on a popup window (or a new tab).
   if (bcl::inside_iframe()) {
@@ -39,14 +37,12 @@ function translateform_buttons($lng, $sguid = NULL) {
   // permissions to submit votes and suggestions.
   $translation_lng = variable_get('btrClient_translation_lng', 'all');
   $enable_save = ($translation_lng == 'all' or ($translation_lng == $lng));
-  $buttons['save'] = array(
+  $buttons['save'] = [
     '#type' => 'submit',
     '#value' => t('Save'),
     '#access' => $enable_save,
-    '#attributes' => array(
-      'onclick' => 'this.form.target="_self"',
-    ),
-  );
+    '#attributes' => ['onclick' => 'this.form.target="_self"'],
+  ];
   // When the user is not authenticated, clicking Save will redirect
   // to login. When we are inside an iFrame, it is better to do the
   // login on a popup window (or a new tab).
