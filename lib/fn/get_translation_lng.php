b72458b29063e43f8e1e05de7ba0798b1f1f8d43
Dashamir Hoxha <dashohoxha@gmail.com>
2015-06-10 19:03:48 +0000

    Fix translation language by giving priority to user preferencies.

--- lib/fn/get_translation_lng.php
+++ lib/fn/get_translation_lng.php
@@ -11,15 +11,13 @@ use \bcl;
  * Return the language of translations.
  */
 function get_translation_lng() {
-  $lng = variable_get('btrClient_translation_lng', 'fr');
-  if ($lng == 'all') {
-    if (!bcl::user_is_authenticated()) {
-      $lng = 'fr';
-    }
-    else {
-      $btr_profile = bcl::user_get_profile();
-      $lng = $btr_profile['translation_lng'];
-    }
+  if (bcl::user_is_authenticated()) {
+    $btr_profile = bcl::user_get_profile();
+    $lng = $btr_profile['translation_lng'];
+  }
+  else {
+    $lng = variable_get('btrClient_translation_lng', 'fr');
+    if ($lng == 'all')  $lng = 'fr';
   }
   return $lng;
 }
