e93eaf72314a145f455b677995f9914b4bbd8dfc
Dashamir Hoxha <dashohoxha@gmail.com>
2015-08-10 04:20:47 +0000

    Improve the display of statistics (#12).

--- lib/fn/render/project_stats.php
+++ lib/fn/render/project_stats.php
@@ -21,18 +21,49 @@ function render_project_stats($origin, $project, $lng) {
     $stats = array();
   }
 
-  $content['project_stats'] = array(
-    '#theme' => 'item_list',
-    '#items' => array(),
-  );
-  $items[] = format_plural($stats['strings'], '1 string', '@count strings');
-  $items[] = t('!nr translated', ['!nr' => $stats['translated']]);
-  $items[] = t('!nr untranslated', ['!nr' => $stats['untranslated']]);
-  $items[] = format_plural($stats['translations'], '1 translation', '@count translations');
-  $items[] = format_plural($stats['votes'], '1 vote', '@count votes');
-  $items[] = format_plural($stats['contributors'], '1 contributor', '@count contributors');
-  $items[] = format_plural($stats['subscribers'], '1 subscriber', '@count subscribers');
+  // All strings
+  $url_list = "btr/project/$origin/$project/$lng/list";
+  $strings = '<a href="' . $url_list . '">'
+    . format_plural($stats['strings'], '1 string', '@count strings')
+    . '</a>';
 
-  $content['project_stats']['#items'] = $items;
+  // Translated strings.
+  $url_list_translated =
+    url($url_list, ['query' => ['list_mode' => 'translated']]);
+  $percent = ' (' . round($stats['translated'] / $stats['strings'] * 100) . '%) ';
+  $translated = '<a href="' . $url_list_translated . '">'
+    . t('!nr translated', ['!nr' => $stats['translated']]) . $percent
+    . '</a>';
+
+  // Untranslated strings.
+  $url_list_untranslated =
+    url($url_list, ['query' => ['list_mode' => 'untranslated']]);
+  $percent = ' (' . round($stats['untranslated'] / $stats['strings'] * 100) . '%) ';
+  $untranslated = '<a href="' . $url_list_untranslated . '">'
+    . t('!nr untranslated', ['!nr' => $stats['untranslated']]) . $percent
+    . '</a>';
+
+  $item = $strings . '<br/> + ' . $translated . '<br/> + ' . $untranslated;
+  $content['project_stats_1'] = array(
+    '#theme' => 'item_list',
+    '#items' => array($item),
+  );
+
+  $content['project_stats_2'] = array(
+    '#theme' => 'item_list',
+    '#items' => array(
+      format_plural($stats['translations'], '1 translation', '@count translations'),
+      format_plural($stats['votes'], '1 vote', '@count votes'),
+    ),
+  );
+
+  $content['project_stats_3'] = array(
+    '#theme' => 'item_list',
+    '#items' => array(
+      format_plural($stats['contributors'], '1 contributor', '@count contributors'),
+      format_plural($stats['subscribers'], '1 subscriber', '@count subscribers'),
+    ),
+  );
+
   return $content;
 }
